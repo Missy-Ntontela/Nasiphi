1f4714cad46b0a9cb9eead5e419571149fe64537
Missy-Ntontela <2673619@students.wits.ac.za>
2026-03-31 21:39:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 let menuIcon=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
 
-menuIcon.onclick=() =>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
+if(menuIcon && navbar){
+    menuIcon.onclick=() =>{
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
+}
 
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
 
-window.onscroll=()=>{
-    sections.forEach(sec =>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop - 150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
+window.onscroll= () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top>=offset && top<offset+height){
-            navLinks.forEach(links =>{
+        if(top>= offset && top <offset + height){
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 //document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                 let activeLink = document.querySelector('header nav a[href*=' + id + ']');
@@ -29,11 +31,13 @@ window.onscroll=()=>{
         };
     });
     let header=document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY>100);
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
+    if(header){
+        header.classList.toggle('sticky',window.scrollY>100);
+    }
+    if(menuIcon && navbar){
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    }
 };
 
 ScrollReveal({
